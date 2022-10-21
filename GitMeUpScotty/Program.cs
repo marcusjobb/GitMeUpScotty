9f509a25e23d46761f58638817937a7bd38bdfcf
Marcus Medina <medina.jobb@outlook.com>
2022-10-21 11:48:32 +0200

    La till kommentarer

--- GitMeUpScotty/Program.cs
+++ GitMeUpScotty/Program.cs
@@ -3,4 +3,10 @@ Console.WriteLine("Hello, World!");
 
 // 1 - Skapa en repository till projektet
 //     med knappen "Add to Source Control"
-// 2 - 
+// 2 - Ändra inte mappen
+// 3 - Gitignore ska vara "Visual Studio"
+// 4 - Licens kan vara Apache 2 eller MIT
+// 5 - Lägg till Readme
+// 6 - Välj Github konto
+// 7 - Välj Repository namn
+// 8 - Push!
