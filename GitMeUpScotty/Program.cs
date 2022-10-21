38dfcf6c2644bdff4dbc6b1ee9dfb20becb56b8a
Marcus Medina <medina.jobb@outlook.com>
2022-10-21 11:44:50 +0200

    Add project files.

---
+++ GitMeUpScotty/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// 1 - Skapa en repository till projektet
+//     med knappen "Add to Source Control"
+// 2 - 
